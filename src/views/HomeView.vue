5470c0d15bf7919df27763e56972f04247cecd45
Nikolai <50028968+Brodatsky@users.noreply.github.com>
2023-01-01 23:17:15 +0300

    3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h2>
+      Vue’s
+      <a href="https://vuejs.org/" target="_blank" rel="noopener"
+        >official documentation
+      </a>
+      provides you with all information you need to get started.
+    </h2>
+  </div>
 </template>
