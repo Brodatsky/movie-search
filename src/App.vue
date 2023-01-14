397962fe532ccebfab941b04d9088e300eae025b
Nikolai <50028968+Brodatsky@users.noreply.github.com>
2023-01-15 01:15:34 +0300

    search comp

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,40 @@
 <script setup>
-import { ref, watchEffect, onMounted, onUpdated } from "vue";
-
-const title = ref("");
-const year = ref("");
-const page = ref("1");
-const Data = ref(null);
-
-watchEffect(async () => {
-  try {
-    Data.value = null;
-    const res = await fetch(
-      `http://www.omdbapi.com/?s=${title.value}&y=${year.value}&page=${page.value}&apikey=77b8e5a8`
-    );
-    Data.value = await res.json();
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-onUpdated(() => {
-  console.log(Data.value);
-});
+import Search from "./components/Search.vue";
 </script>
 
 <template>
-  <div class="wrapper">
-    <div class="form">
-      <h1>Online Movie Search</h1>
-      <div class="form__top">
-        <input
-          v-model="title"
-          type="text"
-          placeholder="Search"
-          class="form__input form__search"
-          name="title"
-        />
-      </div>
-      <div class="form__bottom">
-        <label for="year" class="form__label">Year:</label>
-        <input
-          v-model="year"
-          type="text"
-          placeholder="Type here"
-          class="form__input"
-          name="year"
-        />
-        <label for="type" class="form__label">Type:</label>
-        <input
-          v-model="type"
-          type="text"
-          placeholder="Type here"
-          class="form__input"
-          name="type"
-        />
+  <div class="header">
+    <h1 class="header__title">Online Movie Search</h1>
+    <Search />
+  </div>
+  <!-- <div class="main">
+    <div class="cards__list">
+      <p v-if="!Data">Loading...</p>
+      <div v-for="card in Data.Search" class="card">
+        {{ card }}
       </div>
     </div>
-  </div>
-  <div class="main">
-    <div class="wrapper">
-      <div class="cards__list">
-        <p v-if="!Data">Loading...</p>
-        <div v-for="card in Data.Search" class="card">
-          {{ card }}
-        </div>
-      </div>
-    </div>
-  </div>
+  </div> -->
 </template>
 
 <style scoped>
-.wrapper {
+.header {
+  padding: 10px;
+  margin: 0 auto;
+  max-width: 1024px;
+  background: rgb(207, 198, 187);
+}
+.header__title {
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 20px;
+}
+.main {
   margin: 0 auto;
   width: 1024px;
-  background-color: rgb(231, 230, 228);
+  background: rgb(243, 243, 243);
 }
 
-.form {
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  min-height: 100px;
-  font-size: 17px;
-  align-items: center;
-  width: 800px;
-}
-.form__top {
-  margin-bottom: 10px;
-}
-.form__bottom {
-  display: flex;
-
-  margin-bottom: 30px;
-}
-.form__search {
-  width: 450px;
-}
-.form__label {
-  margin-right: 10px;
-}
-.form__input {
-  height: 30px;
-  margin-right: 10px;
-}
 .card {
   width: 190px;
   border: 1px solid black;
---
+++ src/components/Search.vue
@@ -0,0 +1,109 @@
+<script setup>
+import { ref, onUpdated, watchEffect } from "vue";
+const title = ref("");
+const year = ref("");
+const page = ref("1");
+const Data = ref(null);
+
+watchEffect(async () => {
+  try {
+    Data.value = null;
+    const res = await fetch(
+      `http://www.omdbapi.com/?s=${title.value}&y=${year.value}&page=${page.value}&apikey=77b8e5a8`
+    );
+    Data.value = await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+onUpdated(() => {
+  console.log(Data.value);
+});
+</script>
+<template>
+  <div class="form">
+    <div class="form__search">
+      <input
+        v-model="title"
+        type="text"
+        placeholder="Search"
+        id="form__title"
+      />
+    </div>
+    <div class="form__options">
+      <div>
+        <label for="form__year" class="form__label">Year</label>
+        <input
+          v-model="year"
+          type="text"
+          placeholder="Type here"
+          id="form__year"
+        />
+      </div>
+      <div>
+        <label for="form__type" class="form__label">Type</label>
+        <input
+          v-model="type"
+          type="text"
+          placeholder="Type here"
+          id="form__type"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  font-size: 17px;
+  max-width: 500px;
+}
+
+.form__search {
+  margin-bottom: 10px;
+  width: 100%;
+}
+
+.form input[type="text"] {
+  width: 100%;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+.form__options {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+.form__options div {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+}
+
+.form__label {
+  padding: 0 0 5px 7px;
+
+  display: inline-block;
+}
+.form__input {
+  height: 30px;
+}
+
+@media screen and (max-width: 600px) {
+  .form__options {
+    flex-direction: column;
+  }
+
+  .form__options div {
+    width: 100%;
+  }
+}
+</style>
