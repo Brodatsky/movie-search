b80a8841eb59a554dd0129e87bf7e0a352386889
Nikolai <50028968+Brodatsky@users.noreply.github.com>
2023-01-28 01:12:51 +0300

    loader

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
-import { ref, watchEffect } from "vue";
+import { ref } from "vue";
+import CardList from "./components/CardList.vue";
 
 const form = ref({
-  search: null,
-  year: null,
-  type: null,
-  page: null,
+  search: "",
+  year: "",
+  type: "",
+  page: 1,
 });
-const data: any = ref("");
 
-async function submit() {
-  const res = await fetch(
-    `http://www.omdbapi.com/?s=${form.value.search}&y=${form.value.year}&page=${form.value.page}&apikey=77b8e5a8`
+const data: any = ref("");
+const isLoading = ref(false);
+
+async function getPage() {
+  isLoading.value = true;
+  let res: any = await fetch(
+    `http://www.omdbapi.com/?s=${form.value.search}&y=${form.value.year}&type=${form.value.type}&page=${form.value.page}&apikey=77b8e5a8`
   );
-  data.value = await res.json();
+  let obj = await res.json();
+  data.value = obj.Search;
+
+  isLoading.value = false;
+}
+
+function nextPage() {
+  form.value.page++;
+  getPage();
+}
+
+function prevPage() {
+  form.value.page < 2 ? form.value.page : form.value.page--;
+  getPage();
 }
 </script>
 
@@ -27,40 +44,44 @@ async function submit() {
           type="text"
           placeholder="Search"
           id="form__title"
+          class="form__option"
         />
       </div>
       <div class="form__options">
         <div>
           <label for="form__year" class="form__label">Year</label>
+
           <input
             v-model="form.year"
-            type="text"
-            placeholder="Type here"
             id="form__year"
+            type="number"
+            min="1900"
+            max="2023"
+            step="1"
+            class="form__option"
           />
         </div>
         <div>
           <label for="form__type" class="form__label">Type</label>
-          <input
-            v-model="form.type"
-            type="text"
-            placeholder="Type here"
-            id="form__type"
-          />
+
+          <select v-model="form.type" id="form__type" class="form__option">
+            <option>Movie</option>
+            <option>Series</option>
+            <option value="" selected>All</option>
+          </select>
         </div>
       </div>
-    </div>
 
-    <button @click="submit">search</button>
+      <button @click="getPage" class="form__btn">Search</button>
+    </div>
   </div>
 
   <div class="main">
-    <div class="cards__list">
-      <p v-if="!data">Loading...</p>
-      <div v-for="card in data.Search" class="card">
-        {{ card }}
-      </div>
-    </div>
+    <p v-if="isLoading">Loading...</p>
+    <CardList :data="data" />
+
+    <button @click="prevPage" class="form__btn">Prev</button>
+    <button @click="nextPage" class="form__btn">Next</button>
   </div>
 </template>
 
@@ -82,6 +103,7 @@ async function submit() {
   flex-direction: column;
   align-items: center;
   margin: 0 auto;
+  margin-bottom: 20px;
   font-size: 17px;
   max-width: 500px;
 }
@@ -91,7 +113,7 @@ async function submit() {
   width: 100%;
 }
 
-.form input[type="text"] {
+.form__option {
   width: 100%;
   padding: 12px 20px;
   border: none;
@@ -103,7 +125,7 @@ async function submit() {
   flex-direction: row;
   width: 100%;
   gap: 10px;
-  margin-bottom: 20px;
+  margin-bottom: 30px;
 }
 .form__options div {
   display: flex;
@@ -113,11 +135,16 @@ async function submit() {
 
 .form__label {
   padding: 0 0 5px 7px;
-
   display: inline-block;
 }
-.form__input {
-  height: 30px;
+
+.form__btn {
+  background: rgb(102, 194, 129);
+  width: 50%;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
 
 @media screen and (max-width: 600px) {
@@ -133,18 +160,5 @@ async function submit() {
   margin: 0 auto;
   width: 1024px;
   background: rgb(243, 243, 243);
-}
-
-.card {
-  width: 190px;
-  border: 1px solid black;
-  margin: 5px;
-  word-wrap: break-word;
-}
-.cards__list {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
 }
 </style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const props = defineProps({
+  data: Object,
+});
+</script>
+
+<template>
+  <div class="cards__list">
+    <div v-for="card in data" class="card">
+      <img :src="card.Poster" :alt="card.Title" />
+      <h3 class="card__title">{{ card.Title }}</h3>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cards__list {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.card {
+  width: 190px;
+  border: 1px solid black;
+  margin: 5px;
+  word-wrap: break-word;
+}
+
+.card__title {
+  color: rgb(148, 47, 69);
+}
+.card img {
+  width: 100%;
+  height: auto;
+}
+</style>
